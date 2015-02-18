a25abbcead4272d38f666a691a82e73763bced3e
David McClure <davidwilliammcclure@gmail.com>
2015-02-17 18:19:59 -0800

    Adds the core Elasticsearch query.

--- public/src/javascripts/stores/texts.js
+++ public/src/javascripts/stores/texts.js
@@ -1,6 +1,7 @@
 
 
 var Fluxxor = require('fluxxor');
+var es = require('elasticsearch');
 
 
 module.exports = Fluxxor.createStore({
@@ -12,13 +13,46 @@ module.exports = Fluxxor.createStore({
 
 
   /**
+   * Spin up the Elasticsearch client.
+   */
+  initialize: function() {
+    this.client = new es.Client({
+      host: 'localhost:9201' // TODO: envify.
+    });
+  },
+
+
+  /**
    * When a new query string is entered.
    *
    * @param {String} query - The query string.
    */
   onQuery: function(query) {
+
     console.log(query);
-    // TODO: Execute the query.
+
+    // TODO|dev
+    this.client.search({
+      index: 'hlom',
+      type: 'record',
+      size: 100,
+      body: {
+        query: {
+          query_string: {
+            query: query
+          }
+        },
+        sort: [
+          {count: {order: 'desc'}},
+          '_score'
+        ]
+      }
+    })
+
+    .then(function(res) {
+      console.log(res);
+    })
+
   }
 
 
