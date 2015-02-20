d106df7a2fb99e816616c3c811ceb0ea177c14a3
David McClure <davidwilliammcclure@gmail.com>
2015-02-20 08:26:23 -0800

    Blocks in a /query action on the backend, since we can't directly hit the raw ES endpoint from a SSL page in production.

--- app/controllers/overview.js
+++ app/controllers/overview.js
@@ -1,10 +1,24 @@
 
 
+/**
+ * Overview /metadata.
+ */
 exports.metadata = function(req, res) {
   res.status(204).send();
 };
 
 
+/**
+ * Overview /show.
+ */
 exports.show = function(req, res) {
   res.render('overview/show');
+};
+
+
+/**
+ * Run a search query.
+ */
+exports.query = function(req, res) {
+  // TODO
 };
