1808a6c168b4f100c5e1120416aa0f855f26e4d1
David McClure <davidwilliammcclure@gmail.com>
2015-03-27 08:03:23 -0700

    Committing the map stub.

---
+++ public/src/javascripts/components/map.js
@@ -0,0 +1,18 @@
+
+
+var Fluxxor = require('fluxxor');
+var React = require('react');
+
+
+module.exports = React.createClass({
+
+
+  /**
+   * Render the map.
+   */
+  render: function() {
+    return <h1>(Map)</h1>;
+  }
+
+
+});
