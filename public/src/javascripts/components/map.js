10b779f71734b58e52e7c3905aeb056cb58e1402
David McClure <davidwilliammcclure@gmail.com>
2015-03-28 17:48:40 -0700

    Spins up a Leaflet map instance.

--- public/src/javascripts/components/map.js
+++ public/src/javascripts/components/map.js
@@ -1,5 +1,6 @@
 
 
+var L = require('leaflet');
 var Fluxxor = require('fluxxor');
 var React = require('react');
 
@@ -8,10 +9,35 @@ module.exports = React.createClass({
 
 
   /**
-   * Render the map.
+   * Render the container.
    */
   render: function() {
-    return <h1>(Map)</h1>;
+    return <div id="map"></div>;
+  },
+
+
+  /**
+   * Start the map.
+   */
+  componentDidMount: function() {
+    this._initLeaflet();
+  },
+
+
+  /**
+   * Initialize the Leaflet instance.
+   */
+  _initLeaflet: function() {
+
+    this.map = L.map(this.getDOMNode());
+
+    // Create an OSM tile layer.
+    var url = '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    L.tileLayer(url).addTo(this.map);
+
+    // TODO: Where to focus?
+    this.map.setView([40.73, -73.93], 6)
+
   }
 
 
