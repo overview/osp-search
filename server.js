20449855cd9ab9a2031c1c8643607b4c151fe600
David McClure <davidwilliammcclure@gmail.com>
2015-03-31 14:54:23 -0700

    Installs node-config, adds a default file.

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ var express = require('express');
 var app = express()
 
 // Configure Express.
-require('./config/express')(app);
+require('./app/config/express')(app);
 
 // Register routes.
-require('./config/routes')(app);
+require('./app/config/routes')(app);
 
 module.exports = app
